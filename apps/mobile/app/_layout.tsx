2233f1b43ebae63e2c79c46d61c6cb98421cbc8f
Carson Davis <cmd52465@gmail.com>
2026-02-25 21:54:29 -0800

    add algolia sync lambda, start algolia search

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,24 +1,39 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
+} from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { liteClient as algoliasearch } from 'algoliasearch/lite';
+import { InstantSearch } from 'react-instantsearch-core';
+
+const searchClient = algoliasearch(
+	process.env.EXPO_PUBLIC_ALGOLIA_APP_ID || '',
+	process.env.EXPO_PUBLIC_ALGOLIA_SEARCH_API_KEY || '',
+);
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+	anchor: '(tabs)',
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider
+			value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+		>
+			<Stack>
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+				<Stack.Screen
+					name="modal"
+					options={{ presentation: 'modal', title: 'Modal' }}
+				/>
+			</Stack>
+			<StatusBar style="auto" />
+		</ThemeProvider>
+	);
 }
